ba6948dc9db6e05548cddc37d799d17f1431d04f
Alex Brown <alexdbrown2526@gmail.com>
2018-09-26 17:17:55 -0500

    added three views-home, pets, and owners

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,17 @@
 console.log('client working');
+
+const petApp = angular.module('PetApp',['ngRoute']);
+
+petApp.config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/home', {
+        templateUrl:'../views/home.html',
+        controller: 'HomeController as vm'
+    }).when('/pets',{
+        templateUrl:'../views/pets.html',
+        controller: 'PetController as vm'
+    }).when("/owners", {
+        templateUrl: '../views/owners.html',
+        controller: 'OwnerController as vm'
+    })
+}]);
+
