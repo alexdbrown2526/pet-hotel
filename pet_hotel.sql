9b14c20b9775db89c76b4a9429cdaae23e62aa07
Alex Brown <alexdbrown2526@gmail.com>
2018-10-01 09:08:35 -0500

    updated tables

--- pet_hotel.sql
+++ pet_hotel.sql
@@ -1,15 +1,22 @@
-CREATE TABLE "pets" (
-    "id",
-    "owner",
-     "pet_name",
-      "breed",
-       "color");
 
 
+       CREATE TABLE "pets" (
+    "id" SERIAL PRIMARY KEY,
+    "owner_id" INTEGER REFERENCES "owners",
+     "pet_name" VARCHAR (50),
+      "breed" VARCHAR (50),
+       "color" VARCHAR (50));
+       
+       ALTER TABLE "pets"
+       UPDATE "checked_in"=0/0/0000;
+       
+       
+       
        CREATE TABLE "owners" (
-           "id",
-           "owner_name",
-           "number_of_pets"
+       "id" SERIAL PRIMARY KEY,
+       "owner_name" VARCHAR (50),
+       "number_of_pets" INTEGER
+       
        );
 
     
